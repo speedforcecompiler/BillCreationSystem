84e3c7780bd5f6ed651cde227c77e4a8a57e1287
speedforcecompiler <khan.sohail898@gmail.com>
2017-09-17 19:16:58 +0530

    Web Started

---
+++ php/dbh.php
@@ -0,0 +1,16 @@
+<?php
+	
+	$username = "root";
+	$password = "";
+	$server = "localhost";
+	$dbname = "enterprise";
+
+	try{
+		$conn = new PDO("mysql:host=$server;dbname=$dbname",$username, $password);
+		 // set the PDO error mode to exception
+	    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	    // echo "Connected successfully";
+	}catch(PDOException $e){
+		echo "Sorry Could not connect to database";
+	}
+?>	
---
+++ php/register.php
@@ -0,0 +1,26 @@
+<?php
+	
+	require_once("dbh.php");
+
+	// initializing variables with post values
+	$gst = "-";
+	$day = $_POST['day'];
+	$month = $_POST['month'];
+	$year = $_POST['year'];
+	$name = $_POST['clientname'];
+	$addr = $_POST['clientaddress'];
+	$gst = $_POST['gstnumber'];
+
+	$stmt = $conn->prepare("INSERT INTO master (billnumber, day, month, year, clientname, clientaddr, gstnumber) VALUES (NULL, ?, ?, ?, ?, ?, ?)");
+
+	$stmt->bindParam(1,$day,PDO::PARAM_STR);
+	$stmt->bindParam(2,$month,PDO::PARAM_STR);
+	$stmt->bindParam(3,$year,PDO::PARAM_STR);
+	$stmt->bindParam(4,$name,PDO::PARAM_STR);
+	$stmt->bindParam(5,$addr,PDO::PARAM_STR);
+	$stmt->bindParam(6,$gst,PDO::PARAM_STR);
+
+	$stmt->execute();
+	
+	header("location:../index.html");
+?>
